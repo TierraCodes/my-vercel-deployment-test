5908c0043edb53fb0838d03255931fa7371dd8d0
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-11-06 22:16:08 -0500

    fourth attempt at listing videos

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 export default async function Home() {
   const videos = await prisma.Video.findMany();
-  const videosEl = videos.map((video)=> <li>{video.name}</li>)
+  const videosEl = videos.map((video) => <li>{video.name}</li>)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {videosEl}
