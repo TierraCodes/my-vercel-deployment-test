e562e2651660f7aaa17ac2b80a63b27286f5cb37
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-11-06 23:36:50 -0500

    test test test

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,13 @@ const prisma = new PrismaClient()
 
 export default async function Home() {
 
-  const videos = await prisma.video.findMany();
+  //const videos = await prisma.video.findMany();
 
-  const videosUl = videos.map((videoItem) => <li key={videoItem.id}>{videoItem.name}</li>);
+  //const videosUl = videos.map((videoItem) => <li key={videoItem.id}>{videoItem.name}</li>);
 
   return (
     <main>
       <h1>List of Videos</h1>
-      {/* {videosUl} */}
     </main>
   );
 }
