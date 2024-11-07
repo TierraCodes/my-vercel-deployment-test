6aa863aebdaaa4868a864f10c5a0a2cc7ecf2587
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-11-06 22:19:33 -0500

    sixth attempt at listing videos

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,9 @@ export default async function Home() {
   const videosEl = videos.map((video) => <li>{video.name}</li>)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {videosEl}
+      <ul>
+        {videosEl}
+      </ul>
     </div>
   );
 }
